68df7938c34c43833767c11edf96668962787d74
Fainstain, Eugene <Eugene.Fainstain@amd.com>
2025-11-13 17:15:25 -0800

    - Changed the name to BetterSketch1 (in 2 places), added a tag "APP_NAME" so it would be easier to find.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.bettersketch"
+        applicationId = "com.example.bettersketch1" // APP_NAME - change name here!
         minSdk = 24
         targetSdk = 36
         versionCode = 1
