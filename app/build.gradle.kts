b93121e887ef32d826acf063cc51d59fe7f6c55a
Fainstain, Eugene <Eugene.Fainstain@amd.com>
2025-12-04 12:45:07 -0800

    - Bump to version16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.bettersketch15" // app_name - change name here!
+        applicationId = "com.example.bettersketch16" // app_name - change name here!
         minSdk = 24
         targetSdk = 36
         versionCode = 1
